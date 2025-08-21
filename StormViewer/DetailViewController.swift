8f3866c064187f2181a60b629349350126382e94
aajuvale <ahmed.juvale@gmail.com>
2025-08-21 13:54:50 -0700

    feat: added obj-c func

--- StormViewer/DetailViewController.swift
+++ StormViewer/DetailViewController.swift
@@ -19,6 +19,10 @@ class DetailViewController: UIViewController {
         title = selectedImage
         navigationItem.largeTitleDisplayMode = .never
 
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        )
+
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
@@ -32,5 +36,17 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8)
+        else {
+            print("No Image found.")
+            return
+        }
+
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
 }
